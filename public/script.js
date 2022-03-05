6a8b9425d868c28a63b6966dd44b10a0dd3371c9
minetaro12 <osawa2003@gmail.com>
2022-03-05 21:03:18 +0900

    モーダル表示に完全変更

--- public/script.js
+++ public/script.js
@@ -17,17 +17,49 @@ const whoisSubmit = function() {
   fetch(`/whois?domain=${domain}`)
     .then((res) => {
       if (!res.ok) {
-        statusText.innerHTML = '<p>Error</p>';
+        whoisResult.innerHTML = '<p>Error</p>';
         return;
       };
       return res.text();
     })
     .then((text) => {
-      whoisResult.innerHTML = `<pre>${text}</pre>`
+      whoisResult.innerHTML = `<pre>${text}</pre>`;
       return
     })
     .catch((error) => {
-      statusText.innerHTML = '<p>Error</p>';
+      whoisResult.innerHTML = '<p>Error</p>';
+      return;
+    });
+};
+
+const digSubmit = function() {
+  if (!digDomain.value) {
+    digResult.innerHTML = 'ドメインを入力してください';
+    return;
+  };
+  const domain = digDomain.value;
+  let typeQuery;
+
+  if (!digType.value) {
+    typeQuery = '&type=any';
+  } else {
+    typeQuery = `&type=${digType.value}`;
+  };
+
+  fetch(`/dig?domain=${domain}${typeQuery}`)
+    .then((res) => {
+      if (!res.ok) {
+        digResult.innerHTML = '<p>Error</p>';
+        return;
+      };
+      return res.text();
+    })
+    .then((text) => {
+      digResult.innerHTML = `<pre>${text}</pre>`;
+      return;
+    })
+    .catch((error) => {
+      digResult.innerHTML = '<p>Error</p>';
       return;
     });
 };
