c7b6046fe7c8822dd8a15b9eb3217b22de53275c
minetaro12 <osawa2003@gmail.com>
2023-08-21 22:39:09 +0900

    現在の場所を明示するように

--- web/src/lib/Header.svelte
+++ web/src/lib/Header.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
 <header class="text-white bg-gray-800 p-4 flex justify-between items-center">
   <h1 class="text-2xl">
     <a href="/">DNS Tools</a>
@@ -5,11 +9,17 @@
   <nav>
     <ul class="flex">
       <li>
-        <a href="/">Whois</a>
+        <a class:now={$page.url.pathname == "/"} href="/">Whois</a>
       </li>
       <li class="ml-2">
-        <a href="/query">Query</a>
+        <a class:now={$page.url.pathname == "/query/"} href="/query/">Query</a>
       </li>
     </ul>
   </nav>
 </header>
+
+<style lang="scss">
+  .now {
+    border-bottom: 2px solid;
+  }
+</style>
