3e88f8dc89a5fa360977e99aa133e3bf3a47bf59
minetaro12 <osawa2003@gmail.com>
2024-04-06 20:34:37 +0900

    フロントのファイルをバイナリに埋め込むように変更

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"dns-tools/handlers"
+	"embed"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 )
+
+//go:embed web/build/*
+var embedFs embed.FS
 
 func main() {
 	httpListen := fmt.Sprintf(":%v", getEnv("PORT", "8000"))
 
 	app := fiber.New()
 
-	app.Static("/", "./web/build")
+	// app.Static("/", "./web/build")
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root:       http.FS(embedFs),
+		PathPrefix: "web/build",
+		Browse:     false,
+	}))
 	app.Post("/whois", handlers.Whois)
 	app.Post("/lookup", handlers.Lookup)
 
